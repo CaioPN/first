26b4b0b10fcef4da81f76c413f82e5b7dff28c8e
CaioPN <62997758+CaioPN@users.noreply.github.com>
2023-07-14 02:01:41 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Você esta recebendo o primeiro sinal do abissal</h1>
+    return <h1>Você esta recebendo o primeiro sinal do abissal!</h1>
 }
 
 export default Home;
